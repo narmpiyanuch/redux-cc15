d7823d0ee6e7ff29655cdb9b8ff2715274512dc2
narmpiyanuch <a.piyanuch@hotmail.com>
2023-10-25 14:29:51 +0700

    finish02

--- 02-post-app(redux-only)/_start/post-users.js
+++ 02-post-app(redux-only)/_start/post-users.js
@@ -1,4 +1,4 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
 
 // post = {id:Number, title:string}
 // posts = Array<post> == Array<{id:Number, title:string}>
@@ -6,6 +6,22 @@ const { createStore } = require("redux");
 //initialState
 const initialState = {
   posts: [],
+};
+
+const initialUserState = {
+  users: [], // Array <{id:number, name:string}>
+};
+
+// Action
+const ADD_USER = "ADD_USER";
+const DELETE_USER = "DELETE_USER";
+
+// Action Creator
+const addUserAction = (id, name) => {
+  return {
+    type: ADD_USER,
+    payload: { id: id, name: name },
+  };
 };
 
 // Action
@@ -46,6 +62,7 @@ const editPostAction = (postId, newTitle) => {
 // Reducer = FN 2 Parameters
 const postsReducer = (state = initialState, action) => {
   if (action.type === ADD_POST) {
+    //console.log(state.posts);
     return { posts: [...state.posts, action.payload] };
   } else if (action.type === DELETE_POST) {
     const newPosts = state.posts.filter((post) => post.id !== action.payload);
@@ -60,18 +77,37 @@ const postsReducer = (state = initialState, action) => {
   return state;
 };
 
+// users Resucer
+const usersReducer = (state = initialUserState, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return { users: [...state.users, action.payload] };
+    default:
+      return state;
+  }
+};
+
+// Combined Reducer
+const rootReducer = combineReducers({
+  posts: postsReducer,
+  users: usersReducer,
+});
+
 // Store
-const store = createStore(postsReducer);
+const store = createStore(rootReducer);
 
 store.subscribe(() => {
   const state = store.getState();
-  console.log(" >> ", state);
+  console.log(" >> ", state.posts);
+  console.log(" >>> ", state.users);
 });
 
 store.dispatch(addPostAction({ id: 1, title: "HTML" }));
+store.dispatch(addUserAction(1, "Codecamp"));
+store.dispatch(addUserAction(2, "Joh Doe"));
 store.dispatch(addPostAction({ id: 2, title: "CSS" }));
-store.dispatch(addPostAction({ id: 3, title: "REACT" }));
-//store.dispatch(deletePostAction(2));
+// store.dispatch(addPostAction({ id: 3, title: "REACT" }));
+// store.dispatch(deletePostAction(2));
 // store.dispatch(deletePostAction(3));
-store.dispatch(editPostAction(2, "Prisma"));
-store.dispatch(editPostAction(1, "Express"));
+// store.dispatch(editPostAction(2, "Prisma"));
+// store.dispatch(editPostAction(1, "Express"));
